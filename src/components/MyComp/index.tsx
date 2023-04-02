4724e2faffebd1507a5aad5ea759a6e4dba2d723
fantasticsoul <zhongzhengkai@gmail.com>
2023-04-02 19:06:22 +0800

    feat: bump hel to v4

--- src/components/MyComp/index.tsx
+++ src/components/MyComp/index.tsx
@@ -1,6 +1,28 @@
 import React from 'react'
 import styled from './style.module.css';
+import { message, Dialog, DatePicker } from 'tdesign-react';
+import { getMayStaticShadowNode } from 'hel-utils';
+import { LIB_NAME } from 'configs/subApp';
+
+const attach = () => getMayStaticShadowNode(LIB_NAME, {});
 
 export default function MyComp() {
-  return <h1 className={styled.card}>hi comp</h1>;
+  const [visible, setVisible] = React.useState(false);
+
+  const onCLick = () => {
+    message.info({
+      content: '来自tdesign-react的提示',
+      attach,
+    });
+  };
+  return <div>
+    <Dialog visible={visible} attach={attach} onCancel={() => setVisible(false)}>
+      test
+    </Dialog>
+    <DatePicker popupProps={{ attach }} />
+
+    <button onClick={() => setVisible(true)}>show dialog</button>
+    <h1 className={styled.card} onClick={onCLick}>hi comp, click me</h1>
+  </div>
+
 }
